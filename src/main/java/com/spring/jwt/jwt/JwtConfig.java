e26394d3c53582dd1386b5e98b7822c57d65d761
DESKTOP\ngoloc <ngothanhloc90@gmail.com>
2023-03-23 14:16:19 +0700

    commit!!!

--- src/main/java/com/spring/jwt/jwt/JwtConfig.java
+++ src/main/java/com/spring/jwt/jwt/JwtConfig.java
@@ -18,7 +18,7 @@ public class JwtConfig {
     @Value("${jwt.prefix:Bearer}")
     private String prefix;
 
-    @Value("${jwt.expiration:#{10}}")
+    @Value("${jwt.expiration:#{60*60}}")
     private Long expiration;
 
     @Value("${jwt.secret:7638792F423F4528482B4D6250655368566D597133743677397A24432646294A}")
